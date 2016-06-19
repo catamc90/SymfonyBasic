637e84573fe78717608e2793c2161d0823cf91e2
ktrzos <k.trzos@jazi.pl>
2016-06-19 19:41:10 +0200

    Formatting change.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -38,7 +38,7 @@ class AppKernel extends Kernel
             new Notimeo\BlocksBundle\BlocksBundle(),
         ];
 
-        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+        if(in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
